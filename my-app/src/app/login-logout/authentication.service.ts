d5bbed1e812177955b2a40cc7710ce056d86dd5c
loeeess <s1101632@student.hsleiden.nl>
2021-01-24 15:04:26 +0100

    Hashing

--- my-app/src/app/login-logout/authentication.service.ts
+++ my-app/src/app/login-logout/authentication.service.ts
@@ -9,15 +9,15 @@ export class AuthenticationService {
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string) {
-    
+    const passwordHashed = sha256(password);
 
-    return this.http.post<any>(environment.apiUrl + '/login', { username: username, password: password })
+    return this.http.post<any>(environment.apiUrl + '/login', { username: username, password: passwordHashed })
       .pipe(map(user => {
         // login successful
         if (user !== null) {
           // store user details in local storage to keep user logged in between page refreshes
           sessionStorage.setItem('currentUser', JSON.stringify(user));
-          sessionStorage.setItem('password', password);
+          sessionStorage.setItem('password', passwordHashed);
           sessionStorage.setItem('username', username);
         }
         return user;
